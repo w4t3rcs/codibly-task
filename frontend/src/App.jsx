77d30f693f8f26fdacb231aec4b2a59aec1e7e4d
w4t3rcs <w4t3rofficial@gmail.com>
2025-06-20 00:06:32 +0200

    Some small fixes to <Main />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import {Main} from "./components/main/Main.jsx";
-import "./App.css";
 import * as React from "react";
 import {ThemeProvider} from "./context/ThemeProvider.jsx";
 import {Header} from "./components/header/Header.jsx";
--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -3,10 +3,8 @@ import * as React from "react";
 
 export const Main = () => {
     return (
-        <div className="bg-gray-200 dark:bg-gray-900 h-screen p-7 transition-all">
-            <main className="bg-white dark:bg-gray-800 dark:text-white shadow-md rounded-lg">
-                <WeatherInput />
-            </main>
-        </div>
+        <main className="bg-white dark:bg-gray-800 dark:text-white shadow-md rounded-lg m-7 transition-all">
+            <WeatherInput />
+        </main>
     )
 }
