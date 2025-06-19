204c9a18a306ff786259304ed836251a5e4a61f0
w4t3rcs <w4t3rofficial@gmail.com>
2025-06-20 00:07:58 +0200

    Some small fixes to <Header />

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import {DarkModeButton} from "./DarkModeButton.jsx";
 
 export const Header = () => {
     return (
-        <header className="transition-all dark:bg-gray-800 w-full flex justify-end p-4">
+        <header className="transition-all bg-white dark:bg-gray-800 w-full flex justify-end p-4">
             <DarkModeButton />
         </header>
     )
