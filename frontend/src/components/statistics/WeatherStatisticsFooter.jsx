6d986bbd99a89ac524338a59c40972ba6ca5e4be
w4t3rcs <w4t3rofficial@gmail.com>
2025-06-19 22:03:55 +0200

    Some small updates

--- frontend/src/components/statistics/WeatherStatisticsFooter.jsx
+++ frontend/src/components/statistics/WeatherStatisticsFooter.jsx
@@ -5,7 +5,7 @@ export const WeatherStatisticsFooter = ({data}) => {
             <p className="text-2xl">Statystyki z poprzedniego tygodnia</p>
             <p className="text-gray-300">Temperatury skrajne: {data.minTemperature}&deg;C - {data.maxTemperature}&deg;C</p>
             <p className="text-gray-300">Średnie ciśnienie: {Math.round(data.averageSurfacePressure * 100)/100} hPa</p>
-            <p className="text-gray-300">Średni czas ekspozycji na słońce: {Math.round(data.averageSunshineDuration * 100)/100} kWh</p>
+            <p className="text-gray-300">Średni czas ekspozycji na słońce: {Math.round(data.averageSunshineDuration * 100)/100} s</p>
             <p className="text-gray-300">Podsumowanie tygodnia: {data.precipitationStatus === "WITH_PRECIPITATION" ? "Z opadami" : "Bez opadów"}</p>
         </footer>
     )
